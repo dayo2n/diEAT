16bf00a7f32c1cd8b8c05a35418b5ed2c9cf00f5
dayo2n <0217dayun@naver.com>
2022-10-03 23:03:14 +0900

    [docs] apply gitignore

---
+++ diEAT_SwiftUI/diEAT_SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  diEAT_SwiftUI
+//
+//  Created by 문다 on 2022/10/03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ diEAT_SwiftUI/diEAT_SwiftUI/diEAT_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  diEAT_SwiftUIApp.swift
+//  diEAT_SwiftUI
+//
+//  Created by 문다 on 2022/10/03.
+//
+
+import SwiftUI
+
+@main
+struct diEAT_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

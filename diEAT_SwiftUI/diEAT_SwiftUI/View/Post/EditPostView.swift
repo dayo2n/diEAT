b452efb604b3588c9e8735b58fea729b9312e13a
dayo2n <0217dayun@naver.com>
2023-01-16 20:35:42 +0900

    [fix] iPad에서 식단 작성시 화면이 짤리는 버그 해결
    UIScreen으로 디바이스 자체의 크기를 기준으로 이미지 사이즈를 지정하던 것에서,
    GeometryReader를 이용해 sheet으로 생성된 View 사이즈를 기준으로 지정

--- diEAT_SwiftUI/diEAT_SwiftUI/View/Post/EditPostView.swift
+++ diEAT_SwiftUI/diEAT_SwiftUI/View/Post/EditPostView.swift
@@ -38,185 +38,187 @@ struct EditPostView: View {
     @FocusState private var focused: Bool
     
     var body: some View {
-        ScrollView {
-            ZStack {
-                VStack {
-                    ZStack {
-                        if editMode {
-                            KFImage(URL(string: post!.imageUrl))
-                                .resizable()
-                                .frame(width: UIScreen.main.bounds.width - 20, height: UIScreen.main.bounds.width - 20)
-                                .scaledToFit()
-                        } else {
-                            if image == nil {
-                                Rectangle()
-                                    .frame(width: UIScreen.main.bounds.width - 20, height: UIScreen.main.bounds.width - 20)
-                                    .foregroundColor(Theme.defaultColor(scheme))
-                            } else if let image = image {
-                                image
+        GeometryReader { geo in
+            ScrollView {
+                ZStack {
+                    VStack {
+                        ZStack {
+                            if editMode {
+                                KFImage(URL(string: post!.imageUrl))
                                     .resizable()
-                                    .frame(width: UIScreen.main.bounds.width - 20, height: UIScreen.main.bounds.width - 20)
+                                    .frame(width: geo.size.width - 20, height: geo.size.width - 20)
                                     .scaledToFit()
-                            }
-                        }
-                    }
-                    .padding(.top, editMode ? 100 : 60)
-                    
-                    Button(action: { imagePickMode.toggle() }, label: {
-                        Text("Select image")
-                            .font(.system(size: 15, weight: .semibold, design: .monospaced))
-                            .padding(.bottom, 10)
-                    })
-                    .sheet(isPresented: $imagePickMode, onDismiss: loadImage, content: { ImagePicker(image: $selectedImage) })
-                    
-                    HStack {
-                        Text("# 식사")
-                            .font(.system(size: 16, weight: .semibold, design: .monospaced))
-                            .foregroundColor(Theme.textColor(scheme))
-                            .padding([.horizontal, .top])
-                        Spacer()
-                    }
-                    
-                    Picker("MealTime", selection: $mealTime) {
-                        ForEach(MealTime.allCases) { time in
-                            Text(time.rawValue.capitalized)
-                                .font(.system(size: 14, weight: .semibold, design: .monospaced))
-                                .foregroundColor(Theme.textColor(scheme))
-                        }
-                    }
-                    .pickerStyle(.segmented)
-                    .padding([.leading, .trailing, .bottom])
-                    
-                    HStack {
-                        Text("# 스티커")
-                            .font(.system(size: 16, weight: .semibold, design: .monospaced))
-                            .foregroundColor(Theme.textColor(scheme))
-                            .padding([.horizontal, .top])
-                        Spacer()
-                    }
-                    
-                    HStack {
-                        Button(action: {
-                            selectedIcon = "exercise"
-                        }, label: {
-                            CustomIcon(iconName: "exercise")
-                                .shadow(color: .orange, radius: selectedIcon == "exercise" ? 5 : 0)
-                        })
-                        Spacer()
-                        Button(action: {
-                            selectedIcon = "elephant"
-                        }, label: {
-                            CustomIcon(iconName: "elephant")
-                                .shadow(color: .orange, radius: selectedIcon == "elephant" ? 5 : 0)
-                        })
-                        Spacer()
-                        Button(action: {
-                            selectedIcon = "full"
-                        }, label: {
-                            CustomIcon(iconName: "full")
-                                .shadow(color: .orange, radius: selectedIcon == "full" ? 5 : 0)
-                        })
-                        Spacer()
-                        Button(action: {
-                            selectedIcon = "pig"
-                        }, label: {
-                            CustomIcon(iconName: "pig")
-                                .shadow(color: .orange, radius: selectedIcon == "pig" ? 5 : 0)
-                        })
-                        
-                    }
-                    
-                    HStack {
-                        Text("# 기록")
-                            .font(.system(size: 16, weight: .semibold, design: .monospaced))
-                            .foregroundColor(Theme.textColor(scheme))
-                            .padding([.horizontal, .top])
-                        Spacer()
-                    }
-                    HStack {
-                        Image(systemName: "pencil")
-                            .font(.system(size: 20))
-                            .foregroundColor(Theme.textColor(scheme))
-                            .padding(.leading)
-                        
-                            TextField("스스로 피드백을 남기는 공간", text: $caption)
-                                .font(.system(size: 14, weight: .semibold, design: .monospaced))
-                                .foregroundColor(Theme.textColor(scheme))
-                                .padding(15)
-                                .border(Theme.defaultColor(scheme), width: 0.7)
-                                .padding()
-                                .focused($focused)
-                                .submitLabel(SubmitLabel.done)
-                                .onSubmit {
-                                    focused.toggle()
-                                }
-                    }.padding(.bottom, 50)
-                    
-                    HStack {
-                        Rectangle()
-                            .frame(height: 300)
-                            .opacity(0.0)
-                    }
-                }
-                if uploadPostProgress {
-                    LinearGradient(colors: [.black.opacity(0.5)], startPoint: .top, endPoint: .bottom)
-                        .ignoresSafeArea()
-                    
-                    ProgressView()
-                        .progressViewStyle(CircularProgressViewStyle())
-                        .scaleEffect(5)
-                }
-            }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarLeading) {
-                    Button(action: { mode.wrappedValue.dismiss() }, label: {
-                        Text(editMode ? "" : "Cancel")
-                            .font(.system(size: 14, weight: .semibold, design: .monospaced))
-                            .foregroundColor(Color.red)
-                    })
-                }
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    Button(action: {
-                        if editMode {
-                            uploadPostProgress = true
-                            viewModel.updatePost(id: "\(post!.id ?? "")", selectedDate: post?.timestamp.dateValue() ?? selectedDate, image: selectedImage!, caption: caption, mealtime: mealTime.rawValue, icon: selectedIcon) { _ in
-
-                                print("=== DEBUG: upload sucess on \(selectedDate)!")
-                                uploadPostProgress = false
-                                mode.wrappedValue.dismiss()
-                            }
-                        } else {
-                            if selectedImage == nil {
-                                popNoImageWarning.toggle()
-                                print("=== DEBUG: no selected image")
-                            } else if selectedIcon.count == 0 {
-                                popNoSelectedIconWarning.toggle()
-                                print("=== DEBUG: no selected icon")
                             } else {
+                                if image == nil {
+                                    Rectangle()
+                                        .frame(width: geo.size.width - 20, height: geo.size.width - 20)
+                                        .foregroundColor(Theme.defaultColor(scheme))
+                                } else if let image = image {
+                                    image
+                                        .resizable()
+                                        .frame(width: geo.size.width - 20, height: geo.size.width - 20)
+                                        .scaledToFit()
+                                }
+                            }
+                        }
+                        .padding(.top, editMode ? 100 : 60)
+                        
+                        Button(action: { imagePickMode.toggle() }, label: {
+                            Text("Select image")
+                                .font(.system(size: 15, weight: .semibold, design: .monospaced))
+                                .padding(.bottom, 10)
+                        })
+                        .sheet(isPresented: $imagePickMode, onDismiss: loadImage, content: { ImagePicker(image: $selectedImage) })
+                        
+                        HStack {
+                            Text("# 식사")
+                                .font(.system(size: 16, weight: .semibold, design: .monospaced))
+                                .foregroundColor(Theme.textColor(scheme))
+                                .padding([.horizontal, .top])
+                            Spacer()
+                        }
+                        
+                        Picker("MealTime", selection: $mealTime) {
+                            ForEach(MealTime.allCases) { time in
+                                Text(time.rawValue.capitalized)
+                                    .font(.system(size: 14, weight: .semibold, design: .monospaced))
+                                    .foregroundColor(Theme.textColor(scheme))
+                            }
+                        }
+                        .pickerStyle(.segmented)
+                        .padding([.leading, .trailing, .bottom])
+                        
+                        HStack {
+                            Text("# 스티커")
+                                .font(.system(size: 16, weight: .semibold, design: .monospaced))
+                                .foregroundColor(Theme.textColor(scheme))
+                                .padding([.horizontal, .top])
+                            Spacer()
+                        }
+                        
+                        HStack {
+                            Button(action: {
+                                selectedIcon = "exercise"
+                            }, label: {
+                                CustomIcon(iconName: "exercise")
+                                    .shadow(color: .orange, radius: selectedIcon == "exercise" ? 5 : 0)
+                            })
+                            Spacer()
+                            Button(action: {
+                                selectedIcon = "elephant"
+                            }, label: {
+                                CustomIcon(iconName: "elephant")
+                                    .shadow(color: .orange, radius: selectedIcon == "elephant" ? 5 : 0)
+                            })
+                            Spacer()
+                            Button(action: {
+                                selectedIcon = "full"
+                            }, label: {
+                                CustomIcon(iconName: "full")
+                                    .shadow(color: .orange, radius: selectedIcon == "full" ? 5 : 0)
+                            })
+                            Spacer()
+                            Button(action: {
+                                selectedIcon = "pig"
+                            }, label: {
+                                CustomIcon(iconName: "pig")
+                                    .shadow(color: .orange, radius: selectedIcon == "pig" ? 5 : 0)
+                            })
+                            
+                        }
+                        
+                        HStack {
+                            Text("# 기록")
+                                .font(.system(size: 16, weight: .semibold, design: .monospaced))
+                                .foregroundColor(Theme.textColor(scheme))
+                                .padding([.horizontal, .top])
+                            Spacer()
+                        }
+                        HStack {
+                            Image(systemName: "pencil")
+                                .font(.system(size: 20))
+                                .foregroundColor(Theme.textColor(scheme))
+                                .padding(.leading)
+                            
+                                TextField("스스로 피드백을 남기는 공간", text: $caption)
+                                    .font(.system(size: 14, weight: .semibold, design: .monospaced))
+                                    .foregroundColor(Theme.textColor(scheme))
+                                    .padding(15)
+                                    .border(Theme.defaultColor(scheme), width: 0.7)
+                                    .padding()
+                                    .focused($focused)
+                                    .submitLabel(SubmitLabel.done)
+                                    .onSubmit {
+                                        focused.toggle()
+                                    }
+                        }.padding(.bottom, 50)
+                        
+                        HStack {
+                            Rectangle()
+                                .frame(height: 300)
+                                .opacity(0.0)
+                        }
+                    }
+                    if uploadPostProgress {
+                        LinearGradient(colors: [.black.opacity(0.5)], startPoint: .top, endPoint: .bottom)
+                            .ignoresSafeArea()
+                        
+                        ProgressView()
+                            .progressViewStyle(CircularProgressViewStyle())
+                            .scaleEffect(5)
+                    }
+                }
+                .toolbar {
+                    ToolbarItem(placement: .navigationBarLeading) {
+                        Button(action: { mode.wrappedValue.dismiss() }, label: {
+                            Text(editMode ? "" : "Cancel")
+                                .font(.system(size: 14, weight: .semibold, design: .monospaced))
+                                .foregroundColor(Color.red)
+                        })
+                    }
+                    ToolbarItem(placement: .navigationBarTrailing) {
+                        Button(action: {
+                            if editMode {
                                 uploadPostProgress = true
-                                viewModel.uploadPost(selectedDate: UTC2KST(date: selectedDate), image: selectedImage!, caption: caption, mealtime: mealTime.rawValue, icon: selectedIcon) { _ in
+                                viewModel.updatePost(id: "\(post!.id ?? "")", selectedDate: post?.timestamp.dateValue() ?? selectedDate, image: selectedImage!, caption: caption, mealtime: mealTime.rawValue, icon: selectedIcon) { _ in
+
                                     print("=== DEBUG: upload sucess on \(selectedDate)!")
                                     uploadPostProgress = false
                                     mode.wrappedValue.dismiss()
                                 }
+                            } else {
+                                if selectedImage == nil {
+                                    popNoImageWarning.toggle()
+                                    print("=== DEBUG: no selected image")
+                                } else if selectedIcon.count == 0 {
+                                    popNoSelectedIconWarning.toggle()
+                                    print("=== DEBUG: no selected icon")
+                                } else {
+                                    uploadPostProgress = true
+                                    viewModel.uploadPost(selectedDate: UTC2KST(date: selectedDate), image: selectedImage!, caption: caption, mealtime: mealTime.rawValue, icon: selectedIcon) { _ in
+                                        print("=== DEBUG: upload sucess on \(selectedDate)!")
+                                        uploadPostProgress = false
+                                        mode.wrappedValue.dismiss()
+                                    }
+                                }
                             }
-                        }
-                    }, label: {
-                        Text(editMode ? "Edit" : "Add")
-                            .font(.system(size: 14, weight: .semibold, design: .monospaced))
-                            .foregroundColor(Theme.textColor(scheme))
-                    })
+                        }, label: {
+                            Text(editMode ? "Edit" : "Add")
+                                .font(.system(size: 14, weight: .semibold, design: .monospaced))
+                                .foregroundColor(Theme.textColor(scheme))
+                        })
+                    }
                 }
-            }
-            .popup(isPresented: $popNoImageWarning, type: .floater(), position: .top, autohideIn: 3) {
-                CustomPopUpView(alertText: "업로드 실패!\n 식단 이미지를 첨부해 주세요 :(", bgColor: .red)
-            }
-            .popup(isPresented: $popNoSelectedIconWarning, type: .floater(), position: .top, autohideIn: 3) {
-                CustomPopUpView(alertText: "업로드 실패!\n 오늘의 스티커를 선택해 주세요 :(", bgColor: .red)
-            }
-            .onAppear() {
-                getExistedLog()
-                if editMode { selectedIcon = post!.icon }
+                .popup(isPresented: $popNoImageWarning, type: .floater(), position: .top, autohideIn: 3) {
+                    CustomPopUpView(alertText: "업로드 실패!\n 식단 이미지를 첨부해 주세요 :(", bgColor: .red)
+                }
+                .popup(isPresented: $popNoSelectedIconWarning, type: .floater(), position: .top, autohideIn: 3) {
+                    CustomPopUpView(alertText: "업로드 실패!\n 오늘의 스티커를 선택해 주세요 :(", bgColor: .red)
+                }
+                .onAppear() {
+                    getExistedLog()
+                    if editMode { selectedIcon = post!.icon }
+                }
             }
         }
         .ignoresSafeArea()

84b7a9df1722784df034175d4d868b99cd4d3bca
dayo2n <0217dayun@naver.com>
2023-03-15 23:30:29 +0900

    [FEAAT][#23] splash view 구현 및 화면 연결

--- diEAT/diEAT/View/Main/ContentView.swift
+++ diEAT/diEAT/View/Main/ContentView.swift
@@ -10,14 +10,46 @@ import SwiftUI
 struct ContentView: View {
     
     @EnvironmentObject var viewModel: AuthViewModel
+    @Environment(\.colorScheme) var scheme
+    
+    @State var isInLoading = true
     
     var body: some View {
-        if viewModel.userSession == nil {
-            LoginView()
-        } else {
-            if let user = viewModel.currentUser {
-                MainView(user: user)
+        ZStack {
+            // Main view screen
+            if viewModel.userSession == nil {
+                LoginView()
+            } else {
+                if let user = viewModel.currentUser {
+                    MainView(user: user)
+                }
             }
+            
+            // Launch screen
+            if isInLoading {
+                launchScreenView
+            }
+        }
+        .onAppear {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 2, execute: {
+                isInLoading.toggle()
+            })
+        }
+    }
+}
+
+extension ContentView {
+    var launchScreenView: some View {
+        ZStack(alignment: .center) {
+//            LinearGradient(gradient: Gradient(colors: [Theme.bgColor(scheme), Theme.defaultColor(scheme)]), startPoint: .top, endPoint: .bottom)
+            LinearGradient(gradient: Gradient(colors: [.white]), startPoint: .top, endPoint: .bottom)
+                .edgesIgnoringSafeArea(.all)
+            
+            
+            Image("launchScreenImage")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 200)
         }
     }
 }
